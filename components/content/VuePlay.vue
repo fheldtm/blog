350c94084ed683263ea13df6cc03d13b7be84c4d
fheldtm <fheldtm@gmail.com-fheldtm>
2023-10-02 04:43:29 +0900

    feat: 블로그 기본 설정 중

--- components/content/VuePlay.vue
+++ components/content/VuePlay.vue
@@ -15,7 +15,7 @@ defineProps({
 iframe {
   width: 100%;
   height: 400px;
-  box-shadow: 0 1px 6px rgba(0, 0, 0, 0.16);
+  box-shadow: 0 3px 10px rgba(0, 0, 0, 0.06);
   margin: 0 0 20px;
   border-radius: 5px;
   overflow: hidden;
